0ce508d33aaf7e953aec9fb18f1ca1abad16dc24
SamM71 <sammocock@gmail.com>
2023-09-20 15:34:30 +0100

    add more data

--- seed.py
+++ seed.py
@@ -6,7 +6,11 @@ print("Dropping database")
 db.create_all()
 print("Creating database")
 
+print("Seeding database")
 entry1 = Team(name="Chelsea", nation="England", tier=1)
+entry2 = Team(name="Swindon Town", nation="England", tier=4)
+entry3 = Team(name="Real Madrid", nation="Spain", tier=1)
+entry4 = Team(name="Paris Saint-Germain", nation="France", tier=1)
 
-db.session.add(entry1)
+db.session.add_all([entry1, entry2, entry3, entry4])
 db.session.commit()
